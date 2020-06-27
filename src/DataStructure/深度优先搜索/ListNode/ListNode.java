b5dbba6dbb629e8198be87ff82df17bc8ccc4a5d
raojiawei <2990791932@qq.com>
2020-06-27 20:23:05 +0800

    说点什么好呢

--- src/DataStructure/深度优先搜索/ListNode/ListNode.java
+++ src/DataStructure/深度优先搜索/ListNode/ListNode.java
@@ -10,21 +10,4 @@ public class ListNode {
     ListNode(int x) {
         val = x;
     }
-
-    public static void main(String[] args) {
-        int x=Integer.MAX_VALUE;
-        int y=10;
-        Byte a=new Byte("11");
-        System.out.println (-y);
-
-//        if(isOverFlow (x,1)){
-//            System.out.println ("溢出");
-//        }
-//        else{
-//            System.out.println ("正常");
-//        }
-    }
-    public static boolean isOverFlow(int x,int y){
-        return x+y<x;
-    }
 }
