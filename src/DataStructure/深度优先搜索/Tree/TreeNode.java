bd3376a8cd841f3c3128849b429b7bc90cc941a9
raojiawei <2990791932@qq.com>
2022-10-10 00:08:17 +0800

    12/20

--- src/DataStructure/深度优先搜索/Tree/TreeNode.java
+++
@@ -1,9 +0,0 @@
-package DataStructure.深度优先搜索.Tree;
-
-public class TreeNode {
-
-    public int val;
-    public TreeNode left;
-    public TreeNode right;
-    public TreeNode(int x) { val = x;}
-}
--- src/DataStructure/深度优先搜索/day_2020_5_26/后继者.java
+++
@@ -1,4 +0,0 @@
-package DataStructure.深度优先搜索.day_2020_5_26;
-
-public class 后继者 {
-}
